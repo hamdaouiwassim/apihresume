d125326fa5e4714fee8868656b46e61255f9a08a
hamdaouiwassim <hamdaouiwassim@gmail.com>
2025-11-20 09:17:02 +0100

    debuging process

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-env', function () {
+    return [
+        'NODE_BINARY_PATH' => getenv('NODE_BINARY_PATH'),
+        'PATH' => getenv('PATH'),
+    ];
+});
